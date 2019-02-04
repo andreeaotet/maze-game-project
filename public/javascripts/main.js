213a4e53fe49697f620fbdec0d0b1cf40fc7a58f
Cosmin <cosminotet@gmail.com>
2019-02-04 18:44:17 +0200

    last changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,28 +1,5 @@
-function getNewRow() {
-    return `<tr>
-                <td><input type="text" name="1" placeholder="x"/></td>
-                <td><input type="text" name="2" placeholder="x"/></td>
-                <td><input type="text" name="3" placeholder="x"/></td>
-            </tr>`;
-}
-
-var borderB = document.getElementsByClassName('b-b')
-var vorderT = document.getElementsByClassName('b-t')
-var borderR = document.getElementsByClassName('b-r')
-var borderL = document.getElementsByClassName('b-l')
-
-// function createCell(border1, border2, border3, border4) {
-//     return `<div class="cell ${border1} ${border2} ${border3} ${border4}">X</div>`;
-// }
-
-
-
-// for (var i = 0; i < 5; i++) {
-//     document.querySelector('body').innerHTML += createCell('b-t', 'b-b');
-// }
-
 function loadMaze() {
-    $.ajax('data/maze.json').done(function (maze) {
+    $.ajax('data/mazeeg.json').done(function (maze) {
         window.maze = maze;
         render(maze);
     });
@@ -38,7 +15,7 @@ function render(maze) {
     var cells = maze[2];
 
     var mainRows = maze.map(function(cells) {
-        console.log('cells', cells);
+        // console.log('cells', cells);
 
         var rows = cells.map(function (cell) {
             //console.log('show cell', cell);
@@ -47,12 +24,12 @@ function render(maze) {
         });
 
         //console.log('rows', rows);
-        console.info(rows.join(''));
+        // console.info(rows.join(''));
 
         return '<div class="row">' + rows.join('') + '</div>';
     });
     
-    console.log(mainRows);
+    // console.log(mainRows);
 
     document.querySelector('.container').innerHTML = mainRows.join('');
 }
