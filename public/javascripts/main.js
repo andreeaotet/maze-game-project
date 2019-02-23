5402f1a8cb82662e9b6db106bce7a74cf7271a2a
daniella_abriham@yahoo.com <daniella_abriham@yahoo.com>
2019-02-23 20:45:09 +0200

    updates

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,7 +2,11 @@ var player = document.createElement("div");
 // element.addClass("player");
 player.classList.add("player");
 
-var currentPosition = 0;
+var castle = document.createElement("div");
+castle.classList.add("castle");
+
+var currentPosition = 26;
+var finishPosition = 99;
 
 
 
@@ -19,6 +23,7 @@ function loadMaze() {
         window.maze = maze;
         render(maze);
         document.querySelectorAll(".cell")[currentPosition].appendChild(player);
+        document.querySelectorAll('.cell')[finishPosition].appendChild(castle);
     });
 }
 
@@ -41,9 +46,6 @@ function render(maze) {
     });
 
     document.querySelector('.container').innerHTML = mainRows.join('');
-
-    // var startingCell = document.querySelector('.cell');
-    // startingCell.classList.add("player")
 }
 
 function getStep(keyCode) {
@@ -56,36 +58,44 @@ function getStep(keyCode) {
         case 38:
            return -10;
         case 39:
-            return 1;
+            return +1;
         case 40:
-            return 10
+            return +10
     }
 }
 
 function canMove(keyCode) {
-    var myBorders = maze[parseInt(currentPosition / 10)][currentPosition % 10];// /10; restul%
+    var currentPositionDivide = parseInt(currentPosition / 10);
+    var currentPositionMod = currentPosition % 10;
+    var myBorders = maze[currentPositionDivide][currentPositionMod];// /10; restul%
+
+    //left
+    var rightBorder = maze[currentPositionDivide][currentPositionMod - 1]
+    // up
+    var bottomBorder = maze[currentPositionDivide - 1][currentPositionMod];
+    // right
+    var leftBorder = maze[currentPositionDivide][currentPositionMod + 1];
+   // bottom
+   var topBorder = maze[currentPositionDivide + 1][currentPositionMod];
+
     console.info('borders', myBorders);
     switch (keyCode) {
-        case 37:
-            return true;
-        case 38:
-           return true;
-        case 39:
-            return !myBorders.includes('r');
-        case 40:
-            return true
+        case 37: /* left */
+            return !myBorders.includes('l') && !rightBorder.includes('r');
+        case 38: /* up */
+           return !myBorders.includes('t') && !bottomBorder.includes('b');
+        case 39: /* right */
+            return !myBorders.includes('r') && !leftBorder.includes('l');
+        case 40: /* down */
+            return !myBorders.includes('b') && !topBorder.includes('t');
     }
 }
 
 function initEvents() {
     document.onkeydown = function(e) {
-        
-
-      currentPosition += getStep(e.keyCode);
-      
+        currentPosition += getStep(e.keyCode);
         document.querySelectorAll(".cell")[currentPosition].appendChild(player);
     };
-
 
 }
 
--- routes/maze-db.js
+++ routes/maze-db.js
@@ -13,11 +13,26 @@ const pool = mysql.createPool({
 router.get('/', function(req, res, next) {
   pool.getConnection(function(err, connection) {
     if(err) throw err;
-    const sql = "SELECT * FROM mazes where id='1'";
+    const sql = "SELECT * FROM mazes where id='2'";
     connection.query(sql, function(err, results) {
       if(err) throw err;
       console.log(results[0]);
       res.json(results[0]);
+    })
+  })
+});
+
+// maze/update
+router.post('/update', function(req, res, next) {
+  var id = req.query.id;
+
+  pool.getConnection(function(err, connection) {
+    if(err) throw err;
+    const sql = `UPDATE mazes WHERE id=${id}`;
+    connection.query(sql, function(err, results) {
+      if(err) throw err;
+      console.log(results[0]);
+      res.json({success: true});
     })
   })
 });
